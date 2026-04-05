a35f1b1a9097cbe896713577d9ca97018a06dab3
Khoroshevskyi <sasha99250@gmail.com>
2026-04-05 18:03:38 -0400

    fixed eido code docs

--- peprs-py/py_src/peprs/eido/__init__.py
+++ peprs-py/py_src/peprs/eido/__init__.py
@@ -1,2 +1,3 @@
-from peprs.peprs.eido import *  # noqa: F403
+# Re-exports are provided by the native Rust extension (registered in sys.modules).
+# Type stubs live in __init__.pyi.
 from peprs._pydantic import schema_from_pydantic, validate_with_pydantic  # noqa: F401
